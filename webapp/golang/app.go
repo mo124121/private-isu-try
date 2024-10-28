b47335c988e37e913c1fef1be3dc97b098e16d9a
mo124121 <101929016+mo124121@users.noreply.github.com>
2024-10-28 22:40:00 +0900

    setup pprotein and prometheus

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -22,6 +22,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
+	"github.com/kaz/pprotein/integration/standalone"
 )
 
 var (
@@ -262,6 +263,11 @@ func getTemplPath(filename string) string {
 
 func getInitialize(w http.ResponseWriter, r *http.Request) {
 	dbInitialize()
+	go func() {
+		if _, err := http.Get("http://isucon-o11y:9000/api/group/collect"); err != nil {
+			log.Printf("failed to communicate with pprotein: %v", err)
+		}
+	}()
 	w.WriteHeader(http.StatusOK)
 }
 
@@ -849,6 +855,8 @@ func main() {
 	r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
 		http.FileServer(http.Dir("../public")).ServeHTTP(w, r)
 	})
+
+	go standalone.Integrate(":19001")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
